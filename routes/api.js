a7aa4391a03b0980835838683be14be15af09706
Jared Su <jaredsu1989@gmail.com>
2019-07-24 15:05:19 +1000

    Update api.js to implement error handler and response

--- routes/api.js
+++ routes/api.js
@@ -16,15 +16,26 @@ module.exports = function (app) {
   var convertHandler = new ConvertHandler();
 
   app.route('/api/convert')
-    .get(function (req, res){
+    .get(function (req, res, next){
       var input = req.query.input;
       var initNum = convertHandler.getNum(input);
       var initUnit = convertHandler.getUnit(input);
       var returnNum = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      //error handler
+      if (returnUnit == 'invalid unit' && initNum == 'invalid number') {
+        return res.send('invalid number and unit');
+      } else if (returnUnit == 'invalid unit') {
+        return res.send('invalid unit');
+      } else if(initNum == 'invalid number') {
+        return res.send('invalid number');
+      }
+        //response
+      return res.json({initNum: initNum, initUnit: initUnit, returnNum: returnNum, returnUnit: returnUnit, string: toString});
       
-      //res.json
+      
+      
     });
     
 };
